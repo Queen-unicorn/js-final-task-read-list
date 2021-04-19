7076d910424d501fdcf8b0bd8047cbf3ebde9c41
Queen-unicorn <demtsurina@gmail.com>
2021-04-19 23:11:29 +0300

    finishing search bar

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,55 +1,7 @@
-import { ErrorHandler } from "./errorHandler";
+import { SearchSection } from "./searchSection";
 
 export class App {
   constructor(fetchApi) {
-    this.searchSection = document.getElementById("search-section__book-list");
-    this.descriptionSection = document.getElementById("descriprion-section");
-
-    this.currentBooksOnPage = [];
-
-    const searchInputField = document.getElementById(
-      "search-section__form__input"
-    );
-    const searchSubmitButton = document.getElementById(
-      "search-section__form__submit"
-    );
-
-    searchSubmitButton.addEventListener("click", () => {
-      const query = searchInputField.value;
-      if (!query) return;
-
-      fetchApi.search(query).then(
-        (fetchedBooks) => {
-          this.processSearchResult(fetchedBooks);
-        },
-        (error) => ErrorHandler(error)
-      );
-    });
-  }
-
-  processSearchResult(fetchedBooks) {
-    console.log(fetchedBooks);
-
-    fetchedBooks.docs.forEach((book) => {
-      book.id = book.key.split("/").pop();
-    });
-
-    this.currentBooksOnPage = fetchedBooks.docs;
-    let booksHTML = "";
-
-    for (let [index, book] of Object.entries(this.currentBooksOnPage)) {
-      booksHTML += `
-        <div class="search-section__book-list__item">
-            <p class="search-section__book-list__item__title">${book.title}</p>
-            <p class="search-section__book-list__item__subtitle">${
-              book.subtitle || ""
-            }</p>
-            <p class="search-section__book-list__item__language">(${
-              book.language
-            })</p>
-        </div>
-      `;
-    }
-    this.searchSection.innerHTML = booksHTML;
+    let searchSection = new SearchSection(fetchApi);
   }
 }
---
+++ src/scripts/searchSection.js
@@ -0,0 +1,113 @@
+import { ErrorHandler } from "./errorHandler";
+
+export class SearchSection {
+  constructor(api) {
+    this.searchSection = document.getElementById("search-section__book-list");
+    this.api = api;
+    this.currentBooksOnPage = [];
+
+    this.query = "";
+    this.currentPage = 0;
+
+    this.searchInputField = document.getElementById(
+      "search-section__form__input"
+    );
+    const searchSubmitButton = document.getElementById(
+      "search-section__form__submit"
+    );
+
+    searchSubmitButton.addEventListener("click", () => this.search());
+    this.searchInputField.addEventListener("keyup", (event) => {
+      if (event.key == "Enter") this.search();
+    });
+  }
+
+  search() {
+    this.currentPage = 0;
+    this.query = this.searchInputField.value;
+    if (!this.query) return;
+
+    this.fetchApi();
+  }
+
+  processSearchResult(fetchedBooks) {
+    console.log(fetchedBooks);
+
+    fetchedBooks.docs.forEach((book) => {
+      book.id = book.key.split("/").pop();
+    });
+
+    this.currentBooksOnPage = fetchedBooks.docs;
+    let booksHTML = "";
+
+    for (let [index, book] of Object.entries(this.currentBooksOnPage)) {
+      booksHTML += `
+        <div class="search-section__book-list__item">
+            <p class="search-section__book-list__item__title">${book.title}</p>
+            <p class="search-section__book-list__item__subtitle">${
+              book.subtitle || ""
+            }</p>
+            <p class="search-section__book-list__item__language">(${
+              book.language
+            })</p>
+        </div>
+      `;
+    }
+    this.searchSection.innerHTML = booksHTML;
+    this.showSearchFooter(fetchedBooks.numFound);
+  }
+
+  showSearchFooter(foundBooksNum) {
+    const searchFooter = document.getElementById("search-section__info");
+    const footer = `
+        <div id="search-section__info__numbers">
+            <p>Found: ${foundBooksNum}</p>
+            <p>Start: ${this.currentPage * 100}</p>
+            <p>Page size: 100</p>
+        </div>
+        <div id="search-section__info__navigation">
+            <p id="search-section__info__navigation__prev-button">Previous results</p>
+            <p id="search-section__info__navigation__next-button">Next results</p>
+        </div>
+    `;
+    searchFooter.innerHTML = footer;
+
+    const prevButton = document.getElementById(
+      "search-section__info__navigation__prev-button"
+    );
+    const nextButton = document.getElementById(
+      "search-section__info__navigation__next-button"
+    );
+
+    if (this.currentPage === 0) {
+      prevButton.dataset.disabled = true;
+    }
+    if (this.currentPage === Math.ceil(foundBooksNum / 100) - 1) {
+      nextButton.dataset.disabled = true;
+    }
+
+    prevButton.addEventListener("click", () => {
+      if (prevButton.dataset.disabled) return;
+      this.goToPage(this.currentPage - 1);
+    });
+
+    nextButton.addEventListener("click", () => {
+      if (nextButton.dataset.disabled) return;
+      this.goToPage(this.currentPage + 1);
+    });
+  }
+
+  fetchApi() {
+    this.api.search(this.query, this.currentPage + 1).then(
+      (fetchedBooks) => {
+        this.processSearchResult(fetchedBooks);
+      },
+      (error) => ErrorHandler(error)
+    );
+  }
+
+  goToPage(currentPage) {
+    this.currentPage = currentPage;
+    this.fetchApi();
+  }
+}
